4670a27daf06d6be8d3d16443ceff2d739907c68
Alexander_Solomennik <solomennik.ap.work@gmail.com>
2020-09-23 12:52:45 +0300

    LS-13 общее количество лайков десяти самых молодых пользователей

--- topic8/script.sql
+++ topic8/script.sql
@@ -10,3 +10,19 @@ GROUP BY
    profiles.gender 
 ORDER BY
    count_like DESC LIMIT 1;
+
+
+-- 4 Подсчитать общее количество лайков десяти самым молодым пользователям (сколько лайков получили 10 самых молодых пользователей).
+SELECT
+   TIMESTAMPDIFF(YEAR, birthday, NOW()) as age,
+   COUNT(likes.id) AS count_like 
+FROM
+   profiles 
+   LEFT JOIN
+      likes 
+      ON likes.target_id = profiles.user_id 
+      AND likes.target_type_id = 2 
+GROUP BY
+   profiles.user_id 
+ORDER BY
+   profiles.birthday DESC LIMIT 10;
