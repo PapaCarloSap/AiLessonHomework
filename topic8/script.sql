2c7de8f2e6ea1b878412c0fcd964144bfe7ebd70
Alexander_Solomennik <solomennik.ap.work@gmail.com>
2020-09-23 11:06:31 +0300

    LS-12 подсчетколичества лайков переделан

---
+++ topic8/script.sql
@@ -0,0 +1,11 @@
+-- 3 Определить кто больше поставил лайков (всего) - мужчины или женщины?
+SELECT 
+	   profiles.gender,	
+	   COUNT(likes.id) count_like
+  FROM 
+	   likes
+	   JOIN profiles
+ USING (user_id)
+ GROUP BY profiles.gender
+ ORDER BY count_like DESC
+ LIMIT 1;
