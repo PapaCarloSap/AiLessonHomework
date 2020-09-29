d5c819b7a695749a3f76efdc0668f34eff0b5466
Alexander_Solomennik <solomennik.ap.work@gmail.com>
2020-09-29 14:42:42 +0300

    LS-23 Добавил тригеры для запрета записи NULL

--- DbCource/topic9/lesson8_script.sql
+++ DbCource/topic9/lesson8_script.sql
@@ -21,3 +21,39 @@ END//
 DELIMITER ;
 SELECT hello();
 
+-- В таблице products есть два текстовых поля: name с названием товара и description с его описанием. 
+-- Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное значение NULL неприемлема. 
+-- Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены. При попытке присвоить полям NULL-значение необходимо отменить операцию.
+DELIMITER //
+DROP TRIGGER IF EXISTS check_product_name_description_insert//
+CREATE TRIGGER check_product_name_description_insert BEFORE INSERT ON products
+FOR EACH ROW
+BEGIN
+	IF (NEW.name IS NULL AND NEW.description IS NULL) THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Added canceled';
+	END IF;
+END//
+
+DROP TRIGGER IF EXISTS check_product_name_description_update//
+CREATE TRIGGER check_product_name_description_update BEFORE UPDATE ON products
+FOR EACH ROW
+BEGIN
+	IF (NEW.name IS NULL AND OLD.description IS NULL) 
+		OR (OLD.name IS NULL AND NEW.description IS NULL)
+	THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Updated canceled';
+	END IF;
+END//
+DELIMITER ;
+
+INSERT INTO products 
+	(name, description, price, catalog_id)
+VALUES
+	-- (NULL, NULL, 15000,1);	
+	(NULL, 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 15000,1);
+	-- ('Intel Core i9 - 9900K', NULL, 15000,1);
+	-- ('Intel Core i9 - 9900K', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 15000,1);
+-- UPDATE products SET name = NULL WHERE id = 16;	
+UPDATE products SET description = NULL WHERE id = 15;	
+
+select * from products;
