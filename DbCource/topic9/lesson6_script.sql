edfe96c9d8a7a5bec673a3af92c2e10cf81b2c99
Alexander_Solomennik <solomennik.ap.work@gmail.com>
2020-09-29 12:18:19 +0300

    LS-19 Создано представление, которое выводит название name товарной позиции из таблицы products и соответствующее название каталога name из таблицы catalogs.

--- DbCource/topic9/lesson6_script.sql
+++ DbCource/topic9/lesson6_script.sql
@@ -25,3 +25,14 @@ VALUES
 ;
 COMMIT;
 SELECT * FROM users;
+
+-- Создайте представление, которое выводит название name товарной позиции из таблицы products и соответствующее название каталога name из таблицы catalogs.
+DROP VIEW IF EXISTS view_product;
+CREATE VIEW view_product AS 
+	SELECT 
+		products.name AS product_name,
+		catalogs.name AS catalog_name 
+	FROM 
+		products
+		LEFT JOIN catalogs ON products.catalog_id = catalogs.id; 
+SELECT * FROM view_product;
