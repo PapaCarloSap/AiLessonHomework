76083f5887ac7f73dfb40945152b50e5eb392e50
Alexander_Solomennik <gesper82@yandex.ru>
2021-01-18 21:33:41 +0300

    LS-69 Подготовка перед рефакторингом и решением домашки

---
+++ Data_mining/hw5/main.py
@@ -0,0 +1,13 @@
+from scrapy import crawler
+from scrapy import settings
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+from Data_mining.hw5.gb_parse.spiders.autoyoula import AutoyoulaSpider
+
+
+if __name__ == "__main__":
+    crawler_settings = Settings()
+    crawler_settings.setmodule('Data_mining.hw4.gb_parse.settings')
+    crawler_process = CrawlerProcess(settings=crawler_settings)
+    crawler_process.crawl(AutoyoulaSpider)
+    crawler_process.start()
